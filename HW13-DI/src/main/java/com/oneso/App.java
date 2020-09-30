3df2b399bf1fc5f35b1977662d4bff460c1b834c
theoneso <oneso.null@icloud.com>
2020-09-30 16:17:45 +0300

    IoC
    - Add Exception
    - Add Order for config

--- HW13-DI/src/main/java/com/oneso/App.java
+++ HW13-DI/src/main/java/com/oneso/App.java
@@ -6,8 +6,8 @@ import com.oneso.services.GameProcessor;
 
 public class App {
 
-    public static void main(String[] args) {
-        AppComponentsContainer container = new AppComponentsContainerImpl("com.oneso.config");
+    public static void main(String[] args) throws Exception {
+        AppComponentsContainer container = new AppComponentsContainerImpl(false, "com.oneso.config");
         GameProcessor gameProcessor = container.getAppComponent(GameProcessor.class);
         gameProcessor.startGame();
     }
--- HW13-DI/src/main/java/com/oneso/appcontainer/AppComponentsContainerImpl.java
+++ HW13-DI/src/main/java/com/oneso/appcontainer/AppComponentsContainerImpl.java
@@ -3,6 +3,7 @@ package com.oneso.appcontainer;
 import com.oneso.appcontainer.api.AppComponent;
 import com.oneso.appcontainer.api.AppComponentsContainer;
 import com.oneso.appcontainer.api.AppComponentsContainerConfig;
+import com.oneso.exceptions.ComponentException;
 import org.reflections.Reflections;
 import org.reflections.scanners.MethodAnnotationsScanner;
 import org.reflections.scanners.SubTypesScanner;
@@ -22,19 +23,30 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
     private final List<Object> appComponents = new ArrayList<>();
     private final Map<String, Object> appComponentsByName = new HashMap<>();
 
-    public AppComponentsContainerImpl(Class<?> initialConfigClass) {
+    public AppComponentsContainerImpl(Class<?> initialConfigClass)
+            throws ComponentException {
         processConfig(initialConfigClass);
     }
 
-    public AppComponentsContainerImpl(Class<?>... initialConfigClass) {
-        processConfig(initialConfigClass);
+    public AppComponentsContainerImpl(boolean useOrderOfConfig, Class<?>... initialConfigClass)
+            throws ComponentException {
+        if(!useOrderOfConfig) {
+            processConfig(initialConfigClass);
+        } else {
+            processConfigWithOrder(initialConfigClass);
+        }
     }
 
-    public AppComponentsContainerImpl(String packet) {
-        processConfig(packet);
+    public AppComponentsContainerImpl(boolean useOrderOfConfig, String packet)
+            throws ComponentException {
+        if(!useOrderOfConfig) {
+            processConfig(packet);
+        } else {
+            processConfigWithOrder(packet);
+        }
     }
 
-    private void processConfig(Class<?> configClass) {
+    private void processConfig(Class<?> configClass) throws ComponentException {
         checkConfigClass(configClass);
         var reflection = new Reflections(configClass, new MethodAnnotationsScanner());
         List<Method> methods = reflection.getMethodsAnnotatedWith(AppComponent.class).stream()
@@ -47,7 +59,29 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
         }
     }
 
-    private void processConfig(Class<?>... configClasses) {
+    private void processConfigWithOrder(Class<?>... configClasses) throws ComponentException {
+        List<Class<?>> sortedConfigClasses = Arrays.stream(configClasses)
+                .sorted(Comparator.comparingInt(c -> c.getAnnotation(AppComponentsContainerConfig.class).order()))
+                .collect(Collectors.toList());
+
+        for (var aClass : sortedConfigClasses) {
+            processConfig(aClass);
+        }
+    }
+
+    private void processConfigWithOrder(String packet) throws ComponentException {
+        var reflections = new Reflections(packet, new TypeAnnotationsScanner(), new SubTypesScanner());
+        List<Class<?>> sortedConfigClasses = reflections.getTypesAnnotatedWith(AppComponentsContainerConfig.class, true)
+                .stream()
+                .sorted(Comparator.comparingInt(c -> c.getAnnotation(AppComponentsContainerConfig.class).order()))
+                .collect(Collectors.toList());
+
+        for (var aClass : sortedConfigClasses) {
+            processConfig(aClass);
+        }
+    }
+
+    private void processConfig(Class<?>... configClasses) throws ComponentException {
         List<Method> methods = new ArrayList<>();
 
         for (var aClass : configClasses) {
@@ -67,43 +101,35 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
         }
     }
 
-    private void processConfig(String packet) {
+    private void processConfig(String packet) throws ComponentException {
         var reflections = new Reflections(packet, new TypeAnnotationsScanner(), new SubTypesScanner());
         Set<Class<?>> classes = reflections.getTypesAnnotatedWith(AppComponentsContainerConfig.class, true);
 
         processConfig(classes.toArray(new Class<?>[0]));
     }
 
-    private void initComponent(Method method, Class<?> configClass) {
+    private void initComponent(Method method, Class<?> configClass) throws ComponentException {
         try {
+            List<Object> argForMethod = new ArrayList<>();
             Object instance = configClass.getConstructors()[0].newInstance();
             String name = method.getAnnotation(AppComponent.class).name();
             Object component;
 
             if (method.getParameters().length >= 1) {
-                List<Object> argForMethod = new ArrayList<>();
                 Class<?>[] parameterTypes = method.getParameterTypes();
 
                 for (var parameter : parameterTypes) {
-                    for (var temp : appComponents) {
-                        if (parameter.isAssignableFrom(temp.getClass())) {
-                            argForMethod.add(temp);
-                        }
-                    }
+                    argForMethod.add(getAppComponent(parameter));
                 }
-
-                component = method.invoke(instance, argForMethod.toArray());
-
-            } else {
-                component = method.invoke(instance);
             }
 
+            component = method.invoke(instance, argForMethod.toArray());
             appComponents.add(component);
             appComponentsByName.put(name, component);
 
         } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
             logger.error("Cannot initialize components");
-            e.printStackTrace();
+            throw new ComponentException("Cannot initialize component", e);
         }
     }
 
